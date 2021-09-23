df6070ff0031d211823d6611dac6c9459842fcad
viktoryboykova <viktoryboykova@list.ru>
2021-09-23 19:58:13 +0300

    Какая-то дичь   [#666]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -2,7 +2,7 @@ package ru.job4j;
 
 public class StudentInfo {
     public static void main(String[] args) {
-        System.out.println ("Викуха любит Кису");
-        System.out.println ("Егор любит Викуху и Кису");
+        System.out.println("Викуха любит Кису");
+        System.out.println("Егор любит Викуху и Кису");
     }
 }
