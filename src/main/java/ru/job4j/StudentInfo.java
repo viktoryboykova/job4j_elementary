37fa5a99798f5940a57f54c5bbe5664fa74b65ce
viktoryboykova <viktoryboykova@list.ru>
2021-09-23 19:22:51 +0300

    Какая-то дичь   [#666]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println ("Викуха любит Кису");
+        System.out.println ("Егор любит Викуху и Кису");
+    }
+}
