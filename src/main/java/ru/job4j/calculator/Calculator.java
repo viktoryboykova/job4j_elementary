6a19f13cef223e08dcb9235354326bac9d6cccb1
viktoryboykova <viktoryboykova@list.ru>
2021-09-24 14:32:00 +0300

    Арифметика   [#3]

---
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -0,0 +1,18 @@
+package ru.job4j.calculator;
+
+public class Calculator {
+    public static void main(String[] args) {
+        int six = 6;
+        int two = 2;
+        int sixDivTwo = six / two;
+        System.out.println(sixDivTwo);
+        int five = 5;
+        int one = 1;
+        int fiveMinusOne = five - one;
+        System.out.println(fiveMinusOne);
+        int four = 4;
+        int seven = 7;
+        int fourTimeSeven = four * seven;
+        System.out.println(fourTimeSeven);
+    }
+}
