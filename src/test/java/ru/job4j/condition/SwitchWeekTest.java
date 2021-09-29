91c2f0e2cd1db520cba8f6e3ea797f8c5c07140d
viktoryboykova <viktoryboykova@list.ru>
2021-09-29 13:07:16 +0300

    Switch

---
+++ src/test/java/ru/job4j/condition/SwitchWeekTest.java
@@ -0,0 +1,17 @@
+package ru.job4j.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SwitchWeekTest {
+
+    @Test
+    public void when1thenMonday() {
+        int day = 1;
+        String expected = "Понедельник";
+        String out = SwitchWeek.nameOfDay(day);
+        Assert.assertEquals(expected, out);
+    }
+}
