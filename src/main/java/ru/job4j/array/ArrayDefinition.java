d3b7711651cf6a09600b9dcbf29f976810797c9d
viktoryboykova <viktoryboykova@list.ru>
2021-10-01 16:41:57 +0300

    Заменила sout на цикл

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -7,8 +7,9 @@ public class ArrayDefinition {
         names[0] = "Егорыч Васильев";
         names[1] = "Киса Васильев";
         names[2] = "Викуха Бойкова";
-        System.out.println(names[0]);
-        System.out.println(names[1]);
-        System.out.println(names[2]);
+        for (int index = 0; index < names.length; index++) {
+            System.out.println(names[index]);
+        }
+
     }
 }
