a76001f3bb2b8396bfae78bcbc9d664adc22d103
viktoryboykova <viktoryboykova@list.ru>
2021-10-01 16:03:34 +0300

    Заполнение массива

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -3,11 +3,12 @@ package ru.job4j.array;
 public class ArrayDefinition {
 
     public static void main(String[] args) {
-        short[] ages = new short[10];
-        System.out.println("Размер массива равен " + ages.length);
-        String[] surnames = new String[100500];
-        System.out.println("Размер массива равен " + surnames.length);
-        float[] prices = new float[40];
-        System.out.println("Размер массива равен " + prices.length);
+        String[] names = new String[3];
+        names[0] = "Егорыч Васильев";
+        names[1] = "Киса Васильев";
+        names[2] = "Викуха Бойкова";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
     }
 }
