d697179f81ff9c9da523c0b48ac873093d2ad359
viktoryboykova <viktoryboykova@list.ru>
2021-10-01 15:15:30 +0300

    Размер массива

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -4,7 +4,10 @@ public class ArrayDefinition {
 
     public static void main(String[] args) {
         short[] ages = new short[10];
+        System.out.println("Размер массива равен " + ages.length);
         String[] surnames = new String[100500];
+        System.out.println("Размер массива равен " + surnames.length);
         float[] prices = new float[40];
+        System.out.println("Размер массива равен " + prices.length);
     }
 }
