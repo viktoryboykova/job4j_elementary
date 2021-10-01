14452d7d6b8c0e7e190fafef588b4631984d54e8
viktoryboykova <viktoryboykova@list.ru>
2021-10-01 16:41:03 +0300

    Массивы и цикл for

---
+++ src/main/java/ru/job4j/array/ArrayLoop.java
@@ -0,0 +1,15 @@
+package ru.job4j.array;
+
+public class ArrayLoop {
+
+    public static void main(String[] args) {
+
+        int[] nums = new int[5];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = i * 2 + 3;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(nums[i]);
+        }
+    }
+}
